9358db80cf19a13157d86a3d827edb7016dc4464
Hüseyin Aydın <huseyinaydin99@gmail.com>
2022-12-12 20:14:08 +0300

    Initial commit

---
+++ src/main/java/com/huseyinaydin/springbootkodluyoruz/SpringBootKodluyoruzApplication.java
@@ -0,0 +1,13 @@
+package com.huseyinaydin.springbootkodluyoruz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKodluyoruzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKodluyoruzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huseyinaydin/springbootkodluyoruz/SpringBootKodluyoruzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huseyinaydin.springbootkodluyoruz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKodluyoruzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
